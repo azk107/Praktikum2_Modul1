6592715815c8f2692b00579c9456fdf16d28629c
azk107 <74516636+azk107@users.noreply.github.com>
2022-10-11 19:22:31 +0800

    Update PRAK109-2210817220009-Azka Amalia.c

--- C/PRAK109-2210817220009-Azka Amalia.c
+++ C/PRAK109-2210817220009-Azka Amalia.c
@@ -5,7 +5,7 @@ int main()
     pasukan = 958730;
     pahlawan = 5;
     printf("Jumlah pasukan yang dibawa Yuzhong = %d\n", pasukan);
-    printf("Jumlah pasukan = %d\n", pahlawan);
+    printf("Jumlah pahlawan = %d\n", pahlawan);
 
     pembagian = pasukan/pahlawan;
     printf("Jumlah pasukan yang harus dikalahkan tiap pahlawan adalah %d pasukan", pembagian);
