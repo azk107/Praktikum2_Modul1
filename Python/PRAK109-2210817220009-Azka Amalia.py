a610d76155d9a96f9231ff5abce8fba249da9fb7
azk107 <74516636+azk107@users.noreply.github.com>
2022-10-11 19:21:45 +0800

    Update PRAK109-2210817220009-Azka Amalia.py

--- Python/PRAK109-2210817220009-Azka Amalia.py
+++ Python/PRAK109-2210817220009-Azka Amalia.py
@@ -1,7 +1,7 @@
 pasukan = 958730
 pahlawan = 5
 print('Jumlah pasukan yang dibawa Yuzhong = %d' %pasukan)
-print('Jumlah pasukan = %d' %pahlawan)
+print('Jumlah pahlawan = %d' %pahlawan)
 
 pembagian = pasukan/pahlawan
 print('Jumlah pasukan yang harus dikalahkan tiap pahlawan adalah %d pasukan' %pembagian)
